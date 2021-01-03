3f997ee421c826a4ef234191ebc4f490c21486b1
llx01 <llx01@itcast.cn>
2021-01-03 11:01:25 +0800

    更新index

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+// $(function() {
+//         //调用getuserInfo获取用户基本信息
+//         getuserInfo()
+//     })
+//     //获取用户信息
+//     // function getuserInfo() {
+//     //     $ajax({
+//     //         method: 'get',
+//     //         url: '/my/userinfo',
+
+// //     })
+// // }
